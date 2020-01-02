c692e0861a93de267915bbdd5c1b55667d995441
AbijahDionysius <59437035+AbijahDionysius@users.noreply.github.com>
2020-01-02 15:42:15 +0530

    Add files via upload

---
+++ romeo.py
@@ -0,0 +1,18 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    words = line.rstrip()
+    split_line = line.split()
+    for unique in split_line:
+        if unique not in lst:
+            lst.append(unique)
+lst.sort()
+print(lst)
+
+
+#8.4 Open the file romeo.txt and read it line by line.
+#For each line, split the line into a list of words using the split() method.
+#The program should build a list of words.
+#For each word on each line check to see if the word is already in the list and if not append it to the list.
+#When the program completes, sort and print the resulting words in alphabetical order. 
