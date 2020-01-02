9fa91fb383689114981779431a869eb306700fee
AbijahDionysius <59437035+AbijahDionysius@users.noreply.github.com>
2020-01-02 15:36:51 +0530

    Update Reg_exp&sum.py

--- Reg_exp&sum.py
+++ Reg_exp&sum.py
@@ -4,3 +4,4 @@ import re
 with open("regex_sum_319771_actual.txt", "r") as f:
     nums = re.findall(r'([0-9]+)', f.read())
     print(sum([int(i) for i in nums]))
+#The code does the pull of numbers from text and sums the numbers of it using Regular expressions
