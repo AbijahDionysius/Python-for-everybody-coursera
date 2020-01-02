d1a232990c287b5e8168908dd9f98aebb9a86718
AbijahDionysius <59437035+AbijahDionysius@users.noreply.github.com>
2020-01-02 15:27:38 +0530

    Update Xml_parse_assign1.py

--- Xml_parse_assign1.py
+++ Xml_parse_assign1.py
@@ -40,29 +40,12 @@ while True:
      total += int(each.text)
     print('Sum:',total)
     break
-#Welcome Abijah Dionysius .R from Using Python to Access Web Data
+#from Using Python to Access Web Data
 
 #Extracting Data from XML
 
-#In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geoxml.py. 
+#In this assignment  will write a Python program somewhat similar to http://www.py4e.com/code3/geoxml.py. 
 #The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file.
-
-#We provide two files for this assignment.
-# One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
-
- #Sample data: http://py4e-data.dr-chuck.net/comments_42.xml (Sum=2553)
- #Actual data: http://py4e-data.dr-chuck.net/comments_319775.xml (Sum ends with 69)
-
-#You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
-
-#Data Format and Approach
-
-#The data consists of a number of names and comment counts in XML as follows:
-
-#<comment>
-  #<name>Matthias</name>
-  #<count>97</count>
-#</comment>
 
 #You are to look through all the <comment> tags and find the <count> values sum the numbers. 
 #The closest sample code that shows how to parse XML is geoxml.py. 
